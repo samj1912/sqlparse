38503c932ef974bbc883139a74594dadd2e01398
Nitish Garg <nitish.garg.6174@gmail.com>
2017-02-07 01:44:33 +0530

    Fix index in accepter.c

--- c/accepter.c
+++ c/accepter.c
@@ -3,66 +3,73 @@
 #include <string.h>
 #include <regex.h>
 
+#define SIZE 42
+char *names[]={NULL,"CREATE","TABLE","INSERT","INTO","SELECT","FROM","WHERE","PRIMARY","FOREIGN","KEY","DEFAULT","NOT","TOKEN_NULL","AND","OR","COMPARATOR","REFERENCES","ORDER","BY","DELETE","DATATYPE","VALUES","AUTO_INCREMENT","ASC","DESC","UNIQUE","IN","TRUE","FALSE","DISTINCT","SET","DROP","ALTER","ADD","COLUMN","ALL_COLUMN","BRACKET_OPEN","BRACKET_CLOSE","IDENTIFIER","INT_LITERAL","STRING_LITERAL","SPACE"};
+
 int main(int argc, char const *argv[])
 {
+    int i;
     char input[1000];
-    regex_t re[50];
+    regex_t re[SIZE];
+
     gets(input);
 
     regcomp(&re[0] , "^create\b"                                     , REG_ICASE | REG_NEWLINE );
-    regcomp(&re[0] , "^table\b"                                      , REG_ICASE | REG_NEWLINE );
-    regcomp(&re[0] , "^insert\b"                                     , REG_ICASE | REG_NEWLINE );
-    regcomp(&re[0] , "^into\b"                                       , REG_ICASE | REG_NEWLINE );
-    regcomp(&re[0] , "^select\b"                                     , REG_ICASE | REG_NEWLINE );
-    regcomp(&re[0] , "^from\b"                                       , REG_ICASE | REG_NEWLINE );
-    regcomp(&re[0] , "^where\b"                                      , REG_ICASE | REG_NEWLINE );
-    regcomp(&re[0] , "^primary\b"                                    , REG_ICASE | REG_NEWLINE );
-    regcomp(&re[0] , "^foreign\b"                                    , REG_ICASE | REG_NEWLINE );
-    regcomp(&re[0] , "^key\b"                                        , REG_ICASE | REG_NEWLINE );
-    regcomp(&re[0] , "^default\b"                                    , REG_ICASE | REG_NEWLINE );
-    regcomp(&re[0] , "^not\b"                                        , REG_ICASE | REG_NEWLINE );
-    regcomp(&re[0] , "^null\b"                                       , REG_ICASE | REG_NEWLINE );
-    regcomp(&re[0] , "^and\b"                                        , REG_ICASE | REG_NEWLINE );
-    regcomp(&re[0] , "^or\b"                                         , REG_ICASE | REG_NEWLINE );
-    regcomp(&re[0] , "^!=|<>|>=|<=|>|<"                              , REG_ICASE | REG_NEWLINE );
-    regcomp(&re[0] , "^references\b"                                 , REG_ICASE | REG_NEWLINE );
-    regcomp(&re[0] , "^order\b"                                      , REG_ICASE | REG_NEWLINE );
-    regcomp(&re[0] , "^by\b"                                         , REG_ICASE | REG_NEWLINE );
-    regcomp(&re[0] , "^delete\b"                                     , REG_ICASE | REG_NEWLINE );
-    regcomp(&re[0] , "^(int|integer|double|char|varchar|bit|text)\b" , REG_ICASE | REG_NEWLINE );
-    regcomp(&re[0] , "^values\b"                                     , REG_ICASE | REG_NEWLINE );
-    regcomp(&re[0] , "^auto_increment\b"                             , REG_ICASE | REG_NEWLINE );
-    regcomp(&re[0] , "^asc\b"                                        , REG_ICASE | REG_NEWLINE );
-    regcomp(&re[0] , "^desc\b"                                       , REG_ICASE | REG_NEWLINE );
-    regcomp(&re[0] , "^unique\b"                                     , REG_ICASE | REG_NEWLINE );
-    regcomp(&re[0] , "^in\b"                                         , REG_ICASE | REG_NEWLINE );
-    regcomp(&re[0] , "^true\b"                                       , REG_ICASE | REG_NEWLINE );
-    regcomp(&re[0] , "^false\b"                                      , REG_ICASE | REG_NEWLINE );
-    regcomp(&re[0] , "^distinct\b"                                   , REG_ICASE | REG_NEWLINE );
-    regcomp(&re[0] , "^set\b"                                        , REG_ICASE | REG_NEWLINE );
-    regcomp(&re[0] , "^drop\b"                                       , REG_ICASE | REG_NEWLINE );
-    regcomp(&re[0] , "^alter\b"                                      , REG_ICASE | REG_NEWLINE );
-    regcomp(&re[0] , "^add\b"                                        , REG_ICASE | REG_NEWLINE );
-    regcomp(&re[0] , "^column\b"                                     , REG_ICASE | REG_NEWLINE );
-    regcomp(&re[0] , "^\*"                                           , REG_ICASE | REG_NEWLINE );
-    regcomp(&re[0] , "^\("                                           , REG_ICASE | REG_NEWLINE );
-    regcomp(&re[0] , "^\)"                                           , REG_ICASE | REG_NEWLINE );
-    regcomp(&re[0] , "^[a-zA-Z][a-zA-Z0-9_]*\b"                      , REG_ICASE | REG_NEWLINE );
-    regcomp(&re[0] , "^[+-]?[0-9]+"                                  , REG_ICASE | REG_NEWLINE );
-    regcomp(&re[0] , "^\"[^\"]\""                                    , REG_ICASE | REG_NEWLINE );
-    regcomp(&re[0] , "^[ \t\r\n][ \t\r\n]*"                          , REG_ICASE | REG_NEWLINE );
+    regcomp(&re[1] , "^table\b"                                      , REG_ICASE | REG_NEWLINE );
+    regcomp(&re[2] , "^insert\b"                                     , REG_ICASE | REG_NEWLINE );
+    regcomp(&re[3] , "^into\b"                                       , REG_ICASE | REG_NEWLINE );
+    regcomp(&re[4] , "^select\b"                                     , REG_ICASE | REG_NEWLINE );
+    regcomp(&re[5] , "^from\b"                                       , REG_ICASE | REG_NEWLINE );
+    regcomp(&re[6] , "^where\b"                                      , REG_ICASE | REG_NEWLINE );
+    regcomp(&re[7] , "^primary\b"                                    , REG_ICASE | REG_NEWLINE );
+    regcomp(&re[8] , "^foreign\b"                                    , REG_ICASE | REG_NEWLINE );
+    regcomp(&re[9] , "^key\b"                                        , REG_ICASE | REG_NEWLINE );
+    regcomp(&re[10] , "^default\b"                                    , REG_ICASE | REG_NEWLINE );
+    regcomp(&re[11] , "^not\b"                                        , REG_ICASE | REG_NEWLINE );
+    regcomp(&re[12] , "^null\b"                                       , REG_ICASE | REG_NEWLINE );
+    regcomp(&re[13] , "^and\b"                                        , REG_ICASE | REG_NEWLINE );
+    regcomp(&re[14] , "^or\b"                                         , REG_ICASE | REG_NEWLINE );
+    regcomp(&re[15] , "^!=|<>|>=|<=|>|<"                              , REG_ICASE | REG_NEWLINE );
+    regcomp(&re[16] , "^references\b"                                 , REG_ICASE | REG_NEWLINE );
+    regcomp(&re[17] , "^order\b"                                      , REG_ICASE | REG_NEWLINE );
+    regcomp(&re[18] , "^by\b"                                         , REG_ICASE | REG_NEWLINE );
+    regcomp(&re[19] , "^delete\b"                                     , REG_ICASE | REG_NEWLINE );
+    regcomp(&re[20] , "^(int|integer|double|char|varchar|bit|text)\b" , REG_ICASE | REG_NEWLINE );
+    regcomp(&re[21] , "^values\b"                                     , REG_ICASE | REG_NEWLINE );
+    regcomp(&re[22] , "^auto_increment\b"                             , REG_ICASE | REG_NEWLINE );
+    regcomp(&re[23] , "^asc\b"                                        , REG_ICASE | REG_NEWLINE );
+    regcomp(&re[24] , "^desc\b"                                       , REG_ICASE | REG_NEWLINE );
+    regcomp(&re[25] , "^unique\b"                                     , REG_ICASE | REG_NEWLINE );
+    regcomp(&re[26] , "^in\b"                                         , REG_ICASE | REG_NEWLINE );
+    regcomp(&re[27] , "^true\b"                                       , REG_ICASE | REG_NEWLINE );
+    regcomp(&re[28] , "^false\b"                                      , REG_ICASE | REG_NEWLINE );
+    regcomp(&re[29] , "^distinct\b"                                   , REG_ICASE | REG_NEWLINE );
+    regcomp(&re[30] , "^set\b"                                        , REG_ICASE | REG_NEWLINE );
+    regcomp(&re[31] , "^drop\b"                                       , REG_ICASE | REG_NEWLINE );
+    regcomp(&re[32] , "^alter\b"                                      , REG_ICASE | REG_NEWLINE );
+    regcomp(&re[33] , "^add\b"                                        , REG_ICASE | REG_NEWLINE );
+    regcomp(&re[34] , "^column\b"                                     , REG_ICASE | REG_NEWLINE );
+    regcomp(&re[35] , "^\*"                                           , REG_ICASE | REG_NEWLINE );
+    regcomp(&re[36] , "^\("                                           , REG_ICASE | REG_NEWLINE );
+    regcomp(&re[37] , "^\)"                                           , REG_ICASE | REG_NEWLINE );
+    regcomp(&re[38] , "^[a-zA-Z][a-zA-Z0-9_]*\b"                      , REG_ICASE | REG_NEWLINE );
+    regcomp(&re[39] , "^[+-]?[0-9]+"                                  , REG_ICASE | REG_NEWLINE );
+    regcomp(&re[40] , "^\"[^\"]\""                                    , REG_ICASE | REG_NEWLINE );
+    regcomp(&re[41] , "^[ \t\r\n][ \t\r\n]*"                          , REG_ICASE | REG_NEWLINE );
 
     char pattern[1000];
     strcpy(pattern, input);
 
+    char *match_string = pattern;
     regmatch_t matchptr[10];
-    while(pattern != "")
+
+    while(match_string != "")
     {
-        for(i=0;i<sizeof(re)/sizeof(re[0]);i++)
+        for(i=0;i<SIZE;i++)
         {
-            if(regexec(&re[i], pattern, 10, matchptr, 0) == 0)
+            if(regexec(&re[i], match_string, 10, matchptr, 0) == 0)
             {
-                pattern += matchptr[0].rm_eo;
+                match_string += matchptr[0].rm_eo;
                 printf("%s\n", names[i]);  
             }
             else
---
+++ c/run.sh
@@ -0,0 +1,2 @@
+gcc accepter.c -o accepter
+./accepter < ../input/sample.sql
